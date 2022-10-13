f4a06ff1eae047e02859e54aa671685f2db3aa09
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-13 09:14:41 -0700

    cool coding in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.hi>
+
+/**
+ * main - prints Programming is like building a multilingual puzzle
+ *
+ * Description: prints using put function
+ *
+ * Return: 0 after programme is executed
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
