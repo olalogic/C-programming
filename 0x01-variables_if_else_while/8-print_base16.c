5a3ba30980b19cd85d3cd8df87d6629e5caada4a
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-14 10:39:42 -0700

    Coding conditional statement

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char alphabet;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
