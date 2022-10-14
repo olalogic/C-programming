715130325a65c4394a1b52d433367a930b450c58
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-14 10:07:18 -0700

    Coding conditional statement

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints number
+ *
+ * Description: prints 0123456789
+ *
+ * Return: 0 at the end
+ */
+
+
+int main(void)
+
+{
+	printf("0123456789\n");
+	return (0);
+}
