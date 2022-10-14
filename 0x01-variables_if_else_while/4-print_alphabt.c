478d563329d0534f169a2698755883a3820a63de
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-14 09:54:09 -0700

    Coding conditional statement

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints lower case
+ *
+ * Description: using putchar
+ *
+ * Return: 0 at the end
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'q' & alphabet != 'e')
+			putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
