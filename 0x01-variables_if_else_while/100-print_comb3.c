d6d41aee196203d00bf0f848e4e29c0bda99f5d2
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-14 13:14:03 -0700

    Coding conditional statement

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints lower case
+ *
+ * Description: using putchar
+ *
+ * Return: 0 at the end
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 56 || j != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
