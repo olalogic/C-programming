00d0372388b6b8e2649fc980c32e25e22f9b747a
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-18 14:28:46 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints 
+ *
+ * Description: print _putchar
+ *
+ * Return: 0 after programme is executed
+ */
+int main(void)
+{
+        printf("_putchar\n");
+        return (0);
+}
