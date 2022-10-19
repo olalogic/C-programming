02d5b82ee7d074734d00a2ea37c8a102fb28dd2b
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-18 18:19:01 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @n: character to be checked
+ *
+ * Return: absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
