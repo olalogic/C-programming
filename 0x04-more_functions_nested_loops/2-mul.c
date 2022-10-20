71107cbd8cc6caf6a81d23455e6171ee8cece90e
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-20 15:48:31 -0700

    Cool coding in C

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: character to be multiplied
+ * @b: character to be multiplied
+ * Return: multiplication of a and b
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
