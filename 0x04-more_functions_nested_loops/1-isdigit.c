a49ac125db0688fe26454d6e102ef1bdaccb4a7a
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-20 15:33:47 -0700

    Cool coding in C

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for a digit (0 through 9)
+ *@c: character to be checked
+ * Return: 1 if c is digit, 0 otherwise
+ */
+
+int _isupper(int c)
+
+{
+        return (c >= 0 && c <= 9);
+}
