a2b6f20b74d986767aba6fbbf70cd42e2df5e1cf
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-20 16:02:26 -0700

    Cool coding in C

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers  - prints the numbers, from 0 to 9
+ *Return: 0 after code completion
+ */
+
+void print_numbers(void)
+
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
