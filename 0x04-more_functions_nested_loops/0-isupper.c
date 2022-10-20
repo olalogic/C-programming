ebc5419aadc0d504dda7d09769ba70fde232ef9d
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-20 15:08:54 -0700

    Cool coding in C

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: 0 after programme is executed
+ */
+
+int _isupper(int c)
+
+{
+        
+        if (c >= 'A' && c <= 'Z')
+        {
+                return (1);
+        }
+       	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+functions aamd nested loops
