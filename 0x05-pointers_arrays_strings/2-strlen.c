00831c6411c32eac21615a31bf5ce5046fa3b792
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-24 15:47:13 -0700

    $message

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to check
+ * Return: the length of string s
+ */
+
+int _strlen(char *s)
+{
+	int index;
+
+	index = 0;
+
+	while (s[index] != Null)
+	{
+		i++;
+	}
+
+	return (i);
+}
