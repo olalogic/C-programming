327669e917ec63dfc4706ab6f1cf935e72cc18bf
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-24 16:05:34 -0700

    $message

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse, followed by a new line
+ * print_rev - prints a string in reverse
  * @s: string to be printed
  */
 
@@ -15,8 +15,6 @@ void print_rev(char *s)
 	{
 		i++;
 	}
-
-	
 
 	for (j = i - 1; j >= 0; j--)
 	{
