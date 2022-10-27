1d620f0575960f78bfc73bce699e93247bffaa77
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-27 15:18:32 -0700

    $message

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,18 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n; i++)
-	{
-		if (src[i] != '\0')
-		{
-			dest[i] = src[i];
-		}
-		else if (src[i] = '\0')
-		{
-			dest[i] = '\0';
-		}
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n ; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
+
 
