cfc391ebae9266a2746493cfdec0458b68583ca3
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-10-27 14:50:29 -0700

    $message

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: first string to be concatenated 
+ * @src: the string that will be concatenated
+ * @n: number of characters
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+}
+
