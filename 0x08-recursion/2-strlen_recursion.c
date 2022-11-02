6211e4ba8dd3faba314cc07539ada7938f2fa9cb
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-11-02 15:09:04 -0700

    updated 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: input string
+ * Return: Length of the string as integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+
