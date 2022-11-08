d249db2a066e6c22ef37d1b998e33adbd715367b
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-11-07 16:04:19 -0800

    updated 101-strtow.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,5 +7,9 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
+int get_length(char *str);
+int get_count(char *str);
+char *get_next(char *str);
 
 #endif
