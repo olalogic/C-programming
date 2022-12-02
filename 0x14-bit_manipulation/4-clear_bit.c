e6ed5a0a55a90a7ff0c516177a25b99319f4c6a3
Olaoluwa Idowu <olaoluwajohn.idowu@gmail.com>
2022-12-01 18:50:53 -0800

    updated 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+/**
+ * clear_bit- sets the value of a bit to 0 at a given index.
+ * @n: pointer to a number
+ * @index: index of the bit to set
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+	if ((*n >> index) & 1)
+	{
+		*n -= 1 << index;
+		return (1);
+	}
+	return (1);
+}
+
+
